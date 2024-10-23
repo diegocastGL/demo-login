1c7b22f17aba2a966674af736ccf8842c734adc2
Diego Castellani <diego.castellani@globallogic.com>
2024-10-23 11:26:50 -0300

    Se actualizan diagramas de componente y secuencia

--- src/main/java/com/ejercicio_bci/login/controller/SignUpController.java
+++ src/main/java/com/ejercicio_bci/login/controller/SignUpController.java
@@ -3,6 +3,7 @@ package com.ejercicio_bci.login.controller;
 import com.ejercicio_bci.login.dto.SignUpRequest;
 import com.ejercicio_bci.login.dto.SignUpResponse;
 import com.ejercicio_bci.login.exception.CustomException;
+import com.ejercicio_bci.login.service.SignUpService;
 import com.ejercicio_bci.login.service.impl.SignUpServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sign-up")
 public class SignUpController {
 
-    private SignUpServiceImpl service;
+    private SignUpService service;
 
     @Autowired
     public SignUpController(SignUpServiceImpl service) {
